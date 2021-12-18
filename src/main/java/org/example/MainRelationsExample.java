062830e73ed039b90b689c18309e43955f659f5b
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2021-12-18 13:01:06 +0100

    OneToMany

--- src/main/java/org/example/MainRelationsExample.java
+++ src/main/java/org/example/MainRelationsExample.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.dao.EntityDao;
 import org.example.dao.ReviewerDao;
+import org.example.model.Autor;
 import org.example.model.Movie;
 import org.example.model.Reviewer;
 
@@ -11,28 +12,59 @@ public class MainRelationsExample {
         HibernateFactory hibernateFactory = new HibernateFactory();
         ReviewerDao reviewerDao = new ReviewerDao(hibernateFactory, Reviewer.class);
         EntityDao<Movie> movieDao = new EntityDao<>(hibernateFactory, Movie.class);
+        EntityDao<Autor> autorDao = new EntityDao<>(hibernateFactory, Autor.class);
+
+        savingOneToOne(reviewerDao, movieDao);
+        savingOneToMany(autorDao,movieDao);
 
 
+        hibernateFactory.getSessionFactory().close();
+
+
+    }
+
+    private  static void savingOneToMany(EntityDao<Autor> autorDao, EntityDao<Movie> movieDao) {
+
+        Autor autor = new Autor();
+        autor.setName("Piotr");
+        autor.setSurname("Dorszyk");
+
+        Movie sampleMovieOne = new Movie();
+        sampleMovieOne.setTitle("Titanic");
+        sampleMovieOne.setProductionYear(2000);
+        sampleMovieOne.setType("COMEDY");
+
+        sampleMovieOne.setAutor(autor);
+
+        Movie sampleMovieDwo = new Movie();
+        sampleMovieDwo.setTitle("Psi Patol");
+        sampleMovieDwo.setProductionYear(2005);
+        sampleMovieDwo.setType("Thriller");
+
+        sampleMovieDwo.setAutor(autor);
+
+        autorDao.save(autor);
+        movieDao.save(sampleMovieOne);
+        movieDao.save(sampleMovieDwo);
+
+
+    }
+
+    private static void savingOneToOne(ReviewerDao reviewerDao, EntityDao<Movie> movieDao) {
         Movie sampleMovie = new Movie();
         sampleMovie.setTitle("Sample movie");
         sampleMovie.setProductionYear(1997);
         sampleMovie.setType("DRAMA");
-
 
         Reviewer reviewer = new Reviewer();
         reviewer.setName("Adam");
         reviewer.setSurname("Adamski");
         reviewer.setMovie(sampleMovie);
 
-
         movieDao.save(sampleMovie);
         reviewerDao.save(reviewer);
 
         Reviewer byId = reviewerDao.getById(reviewer.getId());
-
-
-        hibernateFactory.getSessionFactory().close();
-
-
+        System.out.println(byId);
     }
 }
